c3f0a705f1b174a9913e694e3d83480f158b0a44
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2023-04-07 22:00:09 +0500

    Still coding to get the right results

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+/*import { configureStore } from '@reduxjs/toolkit';
 import favoritesReducer from './slice/favoritesSlice';
 
 const store = configureStore({
@@ -6,5 +6,26 @@ const store = configureStore({
     favorites : favoritesReducer,
   },
 });
+
+export default store;*/
+
+import { createStore } from 'redux';
+
+const initialState = {
+  markers: []
+};
+
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_MARKER':
+      return {
+        markers: [...state.markers, action.payload]
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(rootReducer);
 
 export default store;
